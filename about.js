e190efd157cbfa985f309d956716fd2eba9b6c2d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-01-03 14:58:39 +0100

    i made changes to the speakers section and the headline section of the home page

---
+++ about.js
@@ -0,0 +1,20 @@
+const menuOpenIcon = document.getElementById('menu_icon');
+const menuCloseIcon = document.querySelector('.close_menu_icon');
+const menuItems = document.querySelector('.menu_items');
+const li = document.querySelectorAll('.menu_items li');
+
+// functionality to open and close the menu bar
+
+menuOpenIcon.addEventListener('click', () => {
+  menuItems.style.display = 'flex';
+});
+
+menuCloseIcon.addEventListener('click', () => {
+  menuItems.style.display = 'none';
+});
+
+li.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuItems.style.display = 'none';
+  });
+});
--- app.js
+++ app.js
@@ -2,52 +2,81 @@ const speakersData = [
   {
     name: 'David Stones',
     image: './images/david.jpg',
-    title: 'Frontend Engineer',
-    description: '@ Uber',
+    title: 'Frontend Engineer @ Uber',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Gift Akanji',
     image: './images/gift.jpg',
-    title: 'Open source Developer',
-    description: '@ FlightControl',
+    title: 'Open source Developer @ FlightControl',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Michael Strings',
     image: './images/michael.jpg',
-    title: 'Senior Full-Stack Developer',
-    description: '@ Walmart',
+    title: 'Senior Full-Stack Developer @ Walmart',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Anita Burkes',
     image: './images/anita.jpg',
-    title: 'Developer Advocate',
-    description: '@ CloudFlare',
+    title: 'Developer Advocate @ CloudFlare',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Jessica Thomas',
     image: './images/jessica.jpg',
-    title: 'Head Developer Advocate',
-    description: '@ Fusebit',
+    title: 'Head Developer Advocate @ Fusebit',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Lewis Philips',
     image: './images/lewis.jpg',
-    title: 'Frontend Developer',
-    description: '@ Platzi',
+    title: 'Frontend Developer @ Platzi',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Blake Henderson',
     image: './images/Blake.jpg',
-    title: 'Staff Developer Relations Engineer',
-    description: '@ Firebase',
+    title: 'Staff Developer Relations Engineer @ Firebase',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
   {
     name: 'Sarah Nicoletti',
     image: './images/nicoletti.jpg',
-    title: 'Director of Developer Advocacy',
-    description: '@ Cloudinary',
+    title: 'Director of Developer Advocacy @ Cloudinary',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam voluptatem ipsa optio architecto adipisci sequi libero earum nihil',
   },
 ];
+
+const menuOpenIcon = document.getElementById('menu_icon');
+const menuCloseIcon = document.querySelector('.close_menu_icon');
+const menuItems = document.querySelector('.menu_items');
+const li = document.querySelectorAll('.menu_items li');
+
+// functionality to open and close the menu bar
+
+menuOpenIcon.addEventListener('click', () => {
+  menuItems.style.display = 'flex';
+});
+
+menuCloseIcon.addEventListener('click', () => {
+  menuItems.style.display = 'none';
+});
+
+li.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuItems.style.display = 'none';
+  });
+});
 
 const speakersSection = document.getElementById('speakers');
 const speakersHeader = document.createElement('h1');
@@ -74,11 +103,15 @@ const generateMarkUp = () => {
     const speaker = document.createElement('div');
     speaker.className = 'speaker_item';
 
+    const imageContainer = document.createElement('div');
+    const imageBg = document.createElement('div');
+    imageBg.className = 'image_bg';
+    imageContainer.className = 'image_container';
     const speakerImage = document.createElement('img');
     speakerImage.className = 'speaker_image';
     speakerImage.src = image;
-    speakerImage.style.width = '280px';
-    speakerImage.style.height = '200px';
+    // speakerImage.style.width = '200px';
+    // speakerImage.style.height = '160px';
 
     const speakerDetails = document.createElement('div');
     speakerDetails.className = 'speaker_details';
@@ -91,14 +124,20 @@ const generateMarkUp = () => {
     speakerTitle.className = 'speaker_title';
     speakerTitle.textContent = title;
 
+    const headerUnderline = document.createElement('span');
+    headerUnderline.className = 'header_underline';
+
     const speakerDescription = document.createElement('h3');
     speakerDescription.className = 'speaker_description';
     speakerDescription.textContent = description;
 
-    speaker.appendChild(speakerImage);
+    imageContainer.appendChild(speakerImage);
+    imageContainer.appendChild(imageBg);
+    speaker.appendChild(imageContainer);
     speaker.appendChild(speakerDetails);
     speakerDetails.appendChild(speakerName);
     speakerDetails.appendChild(speakerTitle);
+    speakerDetails.appendChild(headerUnderline);
     speakerDetails.appendChild(speakerDescription);
 
     speakersContainer.appendChild(speaker);
